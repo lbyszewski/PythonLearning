3c68175a7a5db8d0b3f6a6b2aedfec7a8156ca62
Lukasz Byszewski <lukasz.byszewski@gmail.com>
2021-05-17 18:00:47 +0200

    Second Edition

--- venv/myOwn.py
+++ venv/myOwn.py
@@ -3,9 +3,25 @@
 x = int(input("podaj liczbę x: "))
 y = int(input("podaj liczbę y: "))
 
-def  getResult(x, y):
+def getFunction(x , y):
+    return x + y
+
+def finalResult(x,y):
     return  x * y
 
-wynik = getResult(x, y)
-print(wynik)
+
+
+def  getResult(x, y):
+    if x >= 3 and y>= 5 :
+        wynik = getFunction(x,y)
+        return print(wynik)
+
+    elif x >= 6 and y >= 8 :
+        wynik2 = finalResult(x,y)
+        return print(wynik2)
+
+
+final = getResult(x,y)
+print(final)
+
 
